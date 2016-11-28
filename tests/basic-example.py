4e5ff51dbfeb833650f9f855a58f51fd4bab7c75
Emilio Coppa <ercoppa@gmail.com>
2016-11-28 18:46:05 +0100

    some testing binaries

---
+++ tests/basic-example.py
@@ -0,0 +1,21 @@
+def start():
+	return 0x400576
+
+def end():
+	return [0x4005bc]
+
+def avoid():
+	return [0x4005d5]
+
+def do_start(state):
+	params = {}
+	params['edi'] = state.regs.edi
+	params['esi'] = state.regs.esi
+	return params
+
+def do_end(state, params):
+	print state.se.any_int(params['esi'])
+	print state.se.any_int(params['edi'])
+	print params['esi']
+	print params['edi']
+	print state.se.constraints
