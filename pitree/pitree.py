ebe4017f2aef36b9fb8c0ab7ae8e728cca613775
camil <demetres@dis.uniroma1.it>
2017-04-05 14:01:04 +0200

    added

--- pitree/pitree.py
+++ pitree/pitree.py
@@ -26,19 +26,31 @@ from cintervaltree import IntervalTree # use custom interval tree
 # page
 # ----------------------------------------------------------------------
 class page:
-    def __init__(self, start, end, tree=None, lazycopy=False):
-        self.start    = start
+
+    def __init__(self, begin, end, tree=None, lazycopy=False):
+        """
+        Page constructor. Intervals [begin, end] are assumed to be closed
+        """
+        self.begin    = begin
         self.end      = end
         self.lazycopy = lazycopy
         if tree is None:
             self.tree  = IntervalTree()
         else: 
             self.tree = tree
-    def __str__(self):
-        return "start="      + str(self.start)    + \
-               ", end="      + str(self.end)      + \
-               ", lazycopy=" + str(self.lazycopy) + \
-               ", tree="     + str(self.tree)
+
+    def _copy_on_write(self):
+        if (self.lazycopy):
+            self.lazycopy = False
+            self.tree = self.tree.copy() # this clones Interval objects in the tree
+        
+    def __repr__(self):
+        return "[begin="     + str(self.begin)      + \
+               ", end="      + str(self.end)        + \
+               ", lazycopy=" + str(self.lazycopy)   + \
+               ", tree="     + str(self.tree) + "]"
+
+    __str__ = __repr__
 
 
 # ----------------------------------------------------------------------
@@ -51,53 +63,66 @@ class pitree:
         self.__lookup    = dict()
         self.__page_size = page_size
 
-    def __str__(self):
-        return str(self.__pages) + " " + str(self.__page_size)
+    def __repr__(self):
+        return str(self.__pages) + "\n\n" + str(self.__lookup) + "\n\n" + str(self.__page_size)
 
     def copy(self):
         """
-        Copy tree
+        Lazy copy of the tree
         :rtype: pitree
         """
         cloned = pitree(self.__page_size)
         for p in self.__lookup.values():
-            n = page(p.start, p.end, p.tree, True)
-            cloned.__pages.addi(n.start, n.end+1, n)
+            n = page(p.begin, p.end, p.tree, True)
+            cloned.__lookup[(p.begin, p.end+1)] = n
+            cloned.__pages.addi(n.begin, n.end+1, n)
         return cloned
 
-    def add(self, start, end, item=None):
+    def add(self, begin, end, item=None):
         """
-        Insert new item with key [start, end].
-        :param start: interval start point (key)
+        Insert new interval with key [begin, end] and value item.
+        :param begin: interval begin point (key)
         :param end: interval end point (key)
         :param item: value associated with key
         """
-        assert start <= end
-        start_p = start / self.__page_size
+        assert begin <= end
+        begin_p = begin / self.__page_size
         end_p   = end   / self.__page_size
         try:
-            p = self.__lookup[(start_p, end_p+1)]
+            p = self.__lookup[(begin_p, end_p+1)]
         except KeyError:
-            p = page(start_p, end_p)
-            self.__lookup[(start_p, end_p+1)] = p
-            self.__pages.addi(p.start, p.end+1, p)
-        if (p.lazycopy):
-            p.lazycopy = False
-            p.tree = p.tree.copy() # *** DEEP COPY HERE! <=================
-        p.tree.addi(start, end, item)
-        print p
+            p = page(begin_p, end_p)
+            self.__lookup[(begin_p, end_p+1)] = p
+            self.__pages.addi(p.begin, p.end+1, p)
+        p._copy_on_write()
+        p.tree.addi(begin, end+1, item)
 
-    def search(self, start, end):
+    def search(self, begin, end):
         """
-        Get all items overlapping with the interval [start, end]
-        :param start: interval start point (key)
+        Get all intervals overlapping with the closed interval [begin, end]
+        :param begin: interval begin point (key)
         :param end: interval end point (key)
-        :rtype: set of items
+        :rtype: set of objects of type Interval (fields: begin, end+1, data)
         """
-        assert start <= end
-        start_p = start / self.__page_size
+        assert begin <= end
+        begin_p = begin / self.__page_size
         end_p   = end   / self.__page_size
         res = set()
-        for i in self.__pages.search(start_p, end_p+1):
-            res.update(i.data.tree.search(start, end+1))
+        for i in self.__pages.search(begin_p, end_p+1):
+            res.update(i.data.tree.search(begin, end+1))
         return res
+
+    def update_item(self, i, new_item):
+        """
+        Update item field of interval in the tree
+        :param i: object of type Interval previously returned by search
+        :param new_item: new value for interval
+        """
+        i.data = new_item
+        begin_p = i.begin / self.__page_size
+        end_p   = i.end   / self.__page_size
+        p = self.__lookup[(begin_p, end_p+1)]
+        p._copy_on_write()
+
+    __str__ = __repr__
+
--- pitree/test.py
+++ pitree/test.py
@@ -9,9 +9,12 @@ def main(args):
     t.add(2400, 3290, "one")
     t.add(1250, 2913, "two")
     t.add(2999, 4600, "three")
-    print t.search(123, 2400)
-#    r = t.copy()
-#    print r
+    t.add(1639, 3007, "four")
+    r = t.copy()
+    for i in r.search(123, 2400):
+        r.update_item(i, i.data + "*")
+        print i.begin, " ", i.end, " ", i.data
+    print r
     return 0
 
 if __name__ == "__main__":
