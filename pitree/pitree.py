c8f2f2d9b3ab484c89b4305f05fef1a44ef5a0f2
camil <demetres@dis.uniroma1.it>
2017-04-06 16:44:00 +0200

    added

--- pitree/pitree.py
+++ pitree/pitree.py
@@ -27,18 +27,27 @@ from cintervaltree import Interval, IntervalTree # use custom interval tree
 # ----------------------------------------------------------------------
 class page:
 
-    def __init__(self, begin, end, tree=None, lazycopy=False):
+    def __init__(self, begin, end):
         """
         Page constructor. Intervals [begin, end] are assumed to be closed
         """
         self.begin    = begin
         self.end      = end
-        self.lazycopy = lazycopy
+        self.lazycopy = False
         self.lookup   = dict()
-        if tree is None:
-            self.tree = IntervalTree()
-        else: 
-            self.tree = tree
+        self.tree     = IntervalTree()
+
+    def copy(self):
+        """
+        Lazy copy of the page - O(1)
+        :rtype: page
+        """
+        self.lazycopy = True
+        p = page(self.begin, self.end)
+        p.lazycopy = True
+        p.tree   = self.tree
+        p.lookup = self.lookup
+        return p
 
     def add(self, begin, end, item=None):
         """
@@ -48,7 +57,7 @@ class page:
         :param item: value associated with key
         """
         self._copy_on_write()
-        i = Interval(begin, end+1, item)
+        i = Interval(begin, end, item)
         self.tree.add(i)
         self.lookup[i] = i
 
@@ -59,11 +68,12 @@ class page:
         :param new_item: new value for interval
         """
         self._copy_on_write()
-        self.lookup[i].data = new_item
+        i = self.lookup[i]
+        i.data = new_item
+        return i
 
     def _copy_on_write(self):
         if (self.lazycopy):
-            print "*** page copy on write: " + str(self)
             self.lazycopy = False
             self.tree = self.tree.copy() # this clones Interval objects in the tree
             self.lookup.clear()
@@ -84,10 +94,10 @@ class page:
 # ----------------------------------------------------------------------
 class pitree:
 
-    def __init__(self, page_size = 1024, lazycopy=False):
+    def __init__(self, page_size = 1024):
         self.__pages     = IntervalTree()
         self.__lookup    = dict()
-        self.__lazycopy  = lazycopy
+        self.__lazycopy  = False
         self.__page_size = page_size
 
     def __repr__(self):
@@ -104,7 +114,8 @@ class pitree:
         :rtype: pitree
         """
         self.__lazycopy = True
-        cloned = pitree(self.__page_size, True)
+        cloned = pitree(self.__page_size)
+        cloned.__lazycopy = True
         cloned.__pages  = self.__pages
         cloned.__lookup = self.__lookup
         return cloned
@@ -118,14 +129,14 @@ class pitree:
         """
         assert begin <= end
         begin_p = begin / self.__page_size
-        end_p   = end   / self.__page_size
+        end_p   = end   / self.__page_size + 1
         self._copy_on_write()
         try:
-            p = self.__lookup[(begin_p, end_p+1)]
+            p = self.__lookup[(begin_p, end_p)]
         except KeyError:
             p = page(begin_p, end_p)
-            self.__lookup[(begin_p, end_p+1)] = p
-            self.__pages.addi(p.begin, p.end+1, p)
+            self.__lookup[(begin_p, end_p)] = p
+            self.__pages.addi(p.begin, p.end, p)
         p.add(begin, end+1, item)
 
     def search(self, begin, end):
@@ -137,10 +148,10 @@ class pitree:
         """
         assert begin <= end
         begin_p = begin / self.__page_size
-        end_p   = end   / self.__page_size
+        end_p   = end   / self.__page_size + 1
         res = set()
-        for i in self.__pages.search(begin_p, end_p+1):
-            res.update(i.data.tree.search(begin, end+1))
+        for i in self.__pages.search(begin_p, end_p):
+            res.update(i.data.tree.search(begin, end))
         return res
 
     def update_item(self, i, new_item):
@@ -151,9 +162,9 @@ class pitree:
         """
         self._copy_on_write()
         begin_p = i.begin / self.__page_size
-        end_p   = i.end   / self.__page_size
-        p = self.__lookup[(begin_p, end_p+1)]
-        p.update_item(i, new_item)
+        end_p   = i.end   / self.__page_size + 1
+        p = self.__lookup[(begin_p, end_p)]
+        return p.update_item(i, new_item)
 
     def _copy_on_write(self):
         """
@@ -164,8 +175,8 @@ class pitree:
             pages  = IntervalTree()
             lookup = dict()
             for p in self.__lookup.values():
-                n = page(p.begin, p.end, p.tree, True)
-                lookup[(p.begin, p.end+1)] = n
-                pages.addi(n.begin, n.end+1, n)
+                n = p.copy()
+                lookup[(p.begin, p.end)] = n
+                pages.addi(n.begin, n.end, n)
             self.__pages  = pages
             self.__lookup = lookup
--- pitree/test.py
+++ pitree/test.py
@@ -6,18 +6,54 @@ from pitree import pitree
 # test
 def main(args):
     t = pitree()
+    t.add(2413, 2414, "zero")
     t.add(2400, 3290, "one")
     t.add(1250, 2913, "two")
-    t.add(2999, 4600, "three")
+    t.add(2999, 4601, "three")
     t.add(1639, 3007, "four")
-    r = t.copy()
-    for i in r.search(123, 2400):
-        r.update_item(i, i.data + "*")
-        print i.begin, " ", i.end, " ", i.data
+
     print "t"
     for i in t.search(0,sys.maxint): print i
+
+    print "update t:"
+    for i in t.search(1250, 1251):
+        i = t.update_item(i, i.data + "---")
+        print i.begin, " ", i.end, " ", i.data
+
+    print "r = copy of t"
+    r = t.copy()
+
+    print "update r:"
+    for i in r.search(123, 2400):
+        i = r.update_item(i, i.data + "*")
+        print i.begin, " ", i.end, " ", i.data
+
+    print "update again t:"
+    for i in t.search(4600, 4600):
+        i = t.update_item(i, i.data + "###")
+        print i.begin, " ", i.end, " ", i.data
+
+    print "t"
+    for i in t.search(0,sys.maxint): print i
+
     print "r"
     for i in r.search(0,sys.maxint): print i
+
+    print "s = copy of r"
+    s = r.copy()
+
+    print "add to s"
+    s.add(113, 1784, "five")
+
+    print "t"
+    for i in t.search(0,sys.maxint): print i
+
+    print "r"
+    for i in r.search(0,sys.maxint): print i
+
+    print "s"
+    for i in s.search(0,sys.maxint): print i
+
     return 0
 
 if __name__ == "__main__":
