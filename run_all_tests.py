87123bf6e7255ef90164f2573596c2860952be38
Emilio Coppa <ercoppa@gmail.com>
2017-04-07 11:54:00 +0200

    updated pseudocode naive v4

--- run_all_tests.py
+++ run_all_tests.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 n = int(sys.argv[1])
-tests = ['basic-example', 'array_O0', 'fail', 'fail2', 'fail3', 'fail4', 'fail5', 'bomb']
+tests = ['basic-example', 'array_O0', 'fail', 'fail2', 'fail3', 'fail4', 'fail5', 'bomb', 'merge']
 
 print
 print "Running tests using memory n=" + str(n)
