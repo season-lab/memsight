e2d99e4ee64b9c64de119c1aa2200bb2fc1aba60
camil <demetres@dis.uniroma1.it>
2017-04-13 23:12:59 +0200

    restructured testing infrastructure

--- pitree/profiler.py
+++ pitree/profiler.py
@@ -8,11 +8,15 @@ class profiler(runner):
     def __init__(self):
         profiler.__init__(self)
 
+    def print_report(self):
+        return
+
 # test
 def main(args):
      print "opening log file %s" % args[0]
      t = profiler()
      t.run(args[0])
+     t.print_report()
      return 0
 
 if __name__ == "__main__":
