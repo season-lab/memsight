8672a243dd9612329b27c919ec9b5ca2e7ca17d1
camil <demetres@dis.uniroma1.it>
2017-04-13 11:44:45 +0200

    fixes

--- pitree/untree.py
+++ pitree/untree.py
@@ -2,38 +2,40 @@ untree_next_id = 1
 
 class Untree(object):
 
-    __round = 0
-    __log = None
+    _round = 0
+    _log   = None
 
     def __init__(self, items=[], log=None, trace=True):
         self._list = items
-        self._log = log
+        self._log  = log
 
         global untree_next_id
         self._id = untree_next_id
         untree_next_id += 1
 
-        self._log(['n', str(self._id)])
+        self._do_log(['n', str(self._id)])
 
     @classmethod
     def set_log(cls, log):
-        assert Untree.__log is None
-        Untree.__log = log
+        assert Untree._log is None
+        Untree._log = log
 
     @classmethod
     def new_round(cls):
-        Untree.__round += 1
-        if Untree.__log is not None:
-            Untree.__log.append(['r', _round])
+        Untree._round += 1
+        if Untree._log is not None:
+            Untree._log.append(['r', Untree._round])
 
-    def _log(self, item):
-        log = self.__log if self.__log is not None else Untree.__log if Untree.__log is not None else None
+    def _do_log(self, item):
+        log = self._log   if self._log   is not None else  \
+              Untree._log if Untree._log is not None else  \
+              None
         if log is not None:
             log.append(item)
 
     def search(self, a, b):
 
-        self._log(['s', str(self._id), str(a), str(b)])
+        self._do_log(['s', str(self._id), str(a), str(b)])
 
         res = []
         for e in self._list:
@@ -44,7 +46,7 @@ class Untree(object):
 
     def update_item(self, e, data):
 
-        self._log(['u', str(self._id), str(id(e.data)), str(id(data))])
+        self._do_log(['u', str(self._id), str(id(e.data)), str(id(data))])
 
         new_e = UntreeItem(e.begin, e.end, data, e.index)
         self._list[e.index] = new_e
@@ -53,14 +55,14 @@ class Untree(object):
 
         r = Untree(self._list[:], log=(self._log[:] if self._log is not None else None), trace=False)
 
-        self._log(['c', str(self._id), str(r._id)])
-        if Untree.__log is None: r._log(['c', str(self._id), str(r._id)])
+        self._do_log(['c', str(self._id), str(r._id)])
+        if Untree._log is None: r._do_log(['c', str(self._id), str(r._id)])
 
         return r
 
     def add(self, begin, end, data):
 
-        self._log(['a', str(self._id), str(begin), str(end), str(id(data))])
+        self._do_log(['a', str(self._id), str(begin), str(end), str(id(data))])
 
         e = UntreeItem(begin, end, data, len(self._list))
         self._list.append(e)
