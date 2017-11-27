57753778ea38df16cbf0f65524591e883c400058
Emilio Coppa <ercoppa@gmail.com>
2017-11-27 16:21:52 +0100

    fixes

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VIRTUALENV_NAME="memsight"
 
+source /usr/local/bin/virtualenvwrapper.sh
+workon $VIRTUALENV_NAME
 python $DIR/run_all_tests.py
 
 exit 0
