8ff22e465293069c89f5b18991cdf77eac7046ce
Emilio Coppa <ercoppa@gmail.com>
2017-11-27 14:30:57 +0100

    travis CI setup

---
+++ build/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+VIRTUALENV_NAME="memsight"
+
+# update apt
+echo "Updating apt..."
+sudo apt-get update >/dev/null || true
+
+# dependencies
+echo "Installing dependencies..."
+sudo apt-get install -y sudo nano python-pip time git
+
+# virtualenv
+echo "Creating virtualenv"
+mkvirtualenv $VIRTUALENV_NAME
+workon memsight
+
+# angr stuff
+echo "Installing angr..."
+sudo pip install -r requirements.txt
+
+# patches
+echo "Applying patches"
+cd ~/.virtualenvs/$VIRTUALENV_NAME/lib/python2.7/site-packages/
+patch -p1 < build/0001-Fix-wrong-ancestry-in-path-merging-issue-761-772.patch
+cd ~
