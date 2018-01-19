6cd5242def2fc1883d64b42a740ad3e8da33dfa5
Emilio Coppa <ercoppa@gmail.com>
2018-01-19 18:05:41 +0100

    fixes

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -10,15 +10,24 @@ source /usr/local/bin/virtualenvwrapper.sh
 workon $VIRTUALENV_NAME
 
 # memsight
-python $DIR/run-all-tests.py
+echo "Test: regression"
+time python $DIR/run-all-tests.py
 
 # pitree
-python $DIR/pitree/test_pitree.py
-python $DIR/pitree/test_intervaltree.py
+echo "Test: pitree"
+time python $DIR/pitree/test_pitree.py
+
+echo "Test: intervaltree"
+time python $DIR/pitree/test_intervaltree.py
 
 # angr examples
-python $DIR/angr-examples/ais3_crackme/solve.py
-python $DIR/angr-examples/asisctffinals2015_license/solve.py
-python $DIR/angr-examples/CADET_00001/solve.py
-python $DIR/angr-examples/cmu_binary_bomb/solve.py
-python $DIR/angr-examples/codegate_2017-angrybird/solve.py
+echo "Test: ais3_crackme"
+time python $DIR/angr-examples/ais3_crackme/solve.py
+echo "Test: asisctffinals2015_license"
+time python $DIR/angr-examples/asisctffinals2015_license/solve.py
+echo "Test: CADET_00001"
+time python $DIR/angr-examples/CADET_00001/solve.py
+echo "Test: cmu_binary_bomb"
+time python $DIR/angr-examples/cmu_binary_bomb/solve.py
+echo "Test: codegate_2017-angrybird"
+time python $DIR/angr-examples/codegate_2017-angrybird/solve.py
