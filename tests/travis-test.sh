ae34c63a09d0f1e785df4d454e9533159c6f32b4
Emilio Coppa <ercoppa@gmail.com>
2018-01-19 19:58:34 +0100

    test: defcamp_r100

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -10,24 +10,26 @@ source /usr/local/bin/virtualenvwrapper.sh
 workon $VIRTUALENV_NAME
 
 # memsight
-echo "Test: regression"
-time python $DIR/run-all-tests.py
+echo -e "\n\nTest: regression"
+python $DIR/run-all-tests.py
 
 # pitree
-echo "Test: pitree"
-time python $DIR/pitree/test_pitree.py
+echo -e "\n\nTest: pitree"
+python $DIR/pitree/test_pitree.py
 
-echo "Test: intervaltree"
-time python $DIR/pitree/test_intervaltree.py
+echo -e "\n\nTest: intervaltree"
+python $DIR/pitree/test_intervaltree.py
 
 # angr examples
-echo "Test: ais3_crackme"
-time python $DIR/angr-examples/ais3_crackme/solve.py
-echo "Test: asisctffinals2015_license"
-time python $DIR/angr-examples/asisctffinals2015_license/solve.py
-echo "Test: CADET_00001"
-time python $DIR/angr-examples/CADET_00001/solve.py
-echo "Test: cmu_binary_bomb"
-time python $DIR/angr-examples/cmu_binary_bomb/solve.py
-echo "Test: codegate_2017-angrybird"
-time python $DIR/angr-examples/codegate_2017-angrybird/solve.py
+echo -e "\n\nTest: ais3_crackme"
+python $DIR/angr-examples/ais3_crackme/solve.py
+echo -e "\n\nTest: asisctffinals2015_license"
+python $DIR/angr-examples/asisctffinals2015_license/solve.py
+echo -e "\n\nTest: CADET_00001"
+python $DIR/angr-examples/CADET_00001/solve.py
+echo -e "\n\nTest: codegate_2017-angrybird"
+python $DIR/angr-examples/codegate_2017-angrybird/solve.py
+echo -e "\n\nTest: defcamp_r100"
+python $DIR/angr-examples/defcamp_r100/solve.py
+echo -e "\n\nTest: cmu_binary_bomb"
+python $DIR/angr-examples/cmu_binary_bomb/solve.py
