197c453b335bdfccd961111ce077fab5b1318a66
Emilio Coppa <ercoppa@gmail.com>
2018-02-15 11:32:18 +0100

    fix script

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -32,4 +32,4 @@ python $DIR/angr-examples/codegate_2017-angrybird/solve.py
 echo -e "\n\nTest: defcamp_r100"
 python $DIR/angr-examples/defcamp_r100/solve.py
 echo -e "\n\nTest: cmu_binary_bomb"
-travis_wait 60 python $DIR/angr-examples/cmu_binary_bomb/solve.py
+python $DIR/angr-examples/cmu_binary_bomb/solve.py
